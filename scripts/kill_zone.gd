34a7ed8e32df57a2803c938aae54e86ceddf8455
nahuel36 <nahuel36gg@gmail.com>
2026-09-05 08:03:23 -0300

    added audio to all kill zones

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@export var audio_stream_player_2d: AudioStreamPlayer2D
 
 
 func _on_body_entered(body: Node2D) -> void:
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	audio_stream_player_2d.play()
 	timer.start()	
 
 func _on_timer_timeout() -> void:
