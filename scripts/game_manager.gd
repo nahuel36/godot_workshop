e87b988a256d26b585d1fb817347c8063e110be3
nahuel36 <nahuel36gg@gmail.com>
2026-09-11 21:09:44 -0300

    *added capacity of win level and change to next

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,8 +3,13 @@ class_name GameManager extends Node
 
 var score = 0
 
+@export var level_score:int 
+@export var next_level:String = "res://scene/game_lvl2.tscn"
 signal OnPointAdded(int)
 
 func add_point():
 	score+=1
 	OnPointAdded.emit(score)
+	if score >= level_score:
+		score = 0
+		get_tree().change_scene_to_file(next_level)
